c8f4405d8abc838a351fcbbd5adab42dc3cdec8b
aaronvern <aaronvernekar@gmail.com>
2024-05-08 20:17:24 +0530

    add frontend

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        customBackground: '#242424',
+      },
+    },
+  },
+  plugins: [],
+}
+
